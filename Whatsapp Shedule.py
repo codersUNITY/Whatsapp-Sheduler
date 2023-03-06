79c08e070aed75259658aff1fa214ccefd2a81d6
RohithvijayR <82448977+RohithvijayR@users.noreply.github.com>
2023-03-06 17:02:11 +0530

    Update Whatsapp Shedule.py

--- Whatsapp Shedule.py
+++ Whatsapp Shedule.py
@@ -1,2 +1,3 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+917358320034' , 'HI da panni  . ', 15, 18)
+pywhatkit.sendwhatmsg(" to phone number " , 'The Message', 15, 18)
+#after "the message" time in 24hrs format, here 15 refers to hour and 18 refer to minute 
